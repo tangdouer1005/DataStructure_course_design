df6c33d04b9fd9b61452923f7ebdbf72e7b8660b
tangdouer1005 <884746853@qq.com>
2023-03-17 20:21:25 +0800

    hello_qt_world

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "DataStructure_course_design.h"
-
 #include <QApplication>
 #pragma comment(lib, "user32.lib")
+#include <QTextStream>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
     DataStructure_course_design w;
+
     w.show();
+
     return a.exec();
 }
