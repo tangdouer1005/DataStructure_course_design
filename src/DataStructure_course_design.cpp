4c042c910cb2f7172eb5fbdc0be1b95860293e70
tangdouer1005 <884746853@qq.com>
2023-03-22 21:35:00 +0800

    通过designer布局了基本框架,但未完成按键链接其他界面

--- src/DataStructure_course_design.cpp
+++ src/DataStructure_course_design.cpp
@@ -4,8 +4,9 @@ DataStructure_course_design::DataStructure_course_design(QWidget *parent)
     : QMainWindow(parent), ui(new Ui_DataStructure_course_design)
 {
     ui->setupUi(this);
+    this->setFixedSize(1106, 556);
     // ui->hello_qt_world->setText("hello_qt_world");
-    ui->hello_qt_world->setText("hello_qt_world");
+    // ui->hello_qt_world->setText("hello_qt_world");
 
     my_debugger = new debug_label(this);
     my_debugger->show();
