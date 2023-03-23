04da9b30a67be7e4767841677d9c0b736ed7d887
tangdouer1005 <884746853@qq.com>
2023-03-23 13:06:56 +0800

    创建闹钟提醒页面,和导航页面, 及其对应ui文件,同时修改了工程文件,使得ui自动生成对应头文件 但并未与主窗口中的按钮链接,而是启动时展示

--- src/DataStructure_course_design.h
+++ src/DataStructure_course_design.h
@@ -2,13 +2,16 @@
 #include "ui_DataStructure_course_design.h"
 #include "debug_label.h"
 #include <QMainWindow>
-
+#include "navigation_window.h"
+#include "alarm_window.h"
 class DataStructure_course_design : public QMainWindow
 {
     Q_OBJECT
 
 public:
     debug_label *my_debugger;
+    navigation_window *my_navigation;
+    alarm_window *my_alarm;
     DataStructure_course_design(QWidget *parent = nullptr);
     ~DataStructure_course_design();
 
